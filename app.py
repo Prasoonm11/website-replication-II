fd91be516cf0bfc56ef8afd12c8ee019c43e755d
Prasoon Mathur <prasoonmathur@Prasoons-MacBook-Air.local>
2026-01-12 10:20:01 +0530

    Fix: Addition of components in Admin Page-33

--- app.py
+++ app.py
@@ -153,7 +153,8 @@ def home():
         contacts=ContactInfo.query.all(),
         cfp_sections=CFPSection.query.order_by(CFPSection.sort_order).all(),
         cfp_points=CFPPoint.query.all(),
-        cfp_buttons=CFPButton.query.all())
+        cfp_buttons=CFPButton.query.all(),
+        about_content=ConferenceAbout.query.all())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
